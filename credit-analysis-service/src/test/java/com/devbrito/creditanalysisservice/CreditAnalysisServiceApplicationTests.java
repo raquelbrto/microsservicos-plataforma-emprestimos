c0de4908bf8add8c24ca7de7b19763106ea9e747
Raquel Brito <raquelbrito662@gmail.com>
2025-04-19 23:50:24 -0300

    feat: initial commit

---
+++ credit-analysis-service/src/test/java/com/devbrito/creditanalysisservice/CreditAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbrito.creditanalysisservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAnalysisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ loan-management-service/src/test/java/com/devbrito/loanmanagementservice/LoanManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbrito.loanmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
